f0bf44417759b7ccc7163651fd4140642e1fac51
MateusRP <mateus.rp.slz@gmail.com>
2024-08-27 21:23:04 -0300

    Função de criptografar pronta

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-var input = document.querySelector(".input");
-var output = document.querySelector(".output");
+function btnEncrypt(){
+    let input = document.querySelector(".input_box");
+    let output = document.querySelector(".output_box");
+    
+    let message = input.value.toLowerCase();
+    
+    message = message.replaceAll("e","enter");
+    message = message.replaceAll("i","imes");
+    message = message.replaceAll("a","ai");
+    message = message.replaceAll("o","ober");
+    message = message.replaceAll("u","ufat");
+
+    output.value = message;
+    input.value = "";
+}
+
+function btnDecrypt(){
+
+}
